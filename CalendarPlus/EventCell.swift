cd92a94c522c99d92b2d32b2a36d6c1ae734c86a
Emil Persson <peem18cz@mace2432-10.jth-stud2.hj.se>
2019-11-15 12:07:51 +0100

    fixed ui, added files for table view

---
+++ CalendarPlus/EventCell.swift
@@ -0,0 +1,19 @@
+//
+//  EventCell.swift
+//  CalendarPlus
+//
+//  Created by Emil Persson on 2019-11-15.
+//  Copyright © 2019 Project_E. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    @IBOutlet weak var UIImageViewEventImage: UIImageView!
+    @IBOutlet weak var UILabelEventTitle: UILabel!
+    @IBOutlet weak var UIImageEventDate: UILabel!
+    
+    
+    
+}
---
+++ CalendarPlus/EventOutput.swift
@@ -0,0 +1,9 @@
+//
+//  EventOutput.swift
+//  CalendarPlus
+//
+//  Created by Emil Persson on 2019-11-15.
+//  Copyright © 2019 Project_E. All rights reserved.
+//
+
+import Foundation
