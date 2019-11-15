4d06153b7e5aeb748df660cbfd254067b883d532
Oliver Köping <kool18dw@mace2432-11.jth-stud2.hj.se>
2019-11-15 12:10:52 +0100

    Added class eventinput and title for storyboard

---
+++ CalendarPlus/EventInput.swift
@@ -0,0 +1,15 @@
+//
+//  EventInput.swift
+//  CalendarPlus
+//
+//  Created by Oliver Köping on 2019-11-15.
+//  Copyright © 2019 Project_E. All rights reserved.
+//
+
+import Foundation
+
+class EventInformation{
+    
+    
+    
+}
