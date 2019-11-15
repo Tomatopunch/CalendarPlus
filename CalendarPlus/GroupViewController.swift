15909b3b35ff1d7acd709422eb18864b67288493
Emil Persson <peem18cz@mace2432-10.jth-stud2.hj.se>
2019-11-15 11:30:18 +0100

    Added all viewcontrollers and ready for dev

---
+++ CalendarPlus/GroupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroupViewController.swift
+//  CalendarPlus
+//
+//  Created by Emil Persson on 2019-11-15.
+//  Copyright © 2019 Project_E. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
