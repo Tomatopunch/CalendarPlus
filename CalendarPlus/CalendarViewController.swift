d9349ca9622a4c551e494dc1c9ea03cbd74fea55
Dennis Andersson <ande1600@mace2432-24.jth-stud2.hj.se>
2019-11-22 09:59:33 +0100

    implemented cocoapods FScalendar

---
+++ CalendarPlus/CalendarViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CalendarViewController.swift
+//  CalendarPlus
+//
+//  Created by Dennis Andersson on 2019-11-22.
+//  Copyright © 2019 Project_E. All rights reserved.
+//
+
+import UIKit
+import FSCalendar
+
+class CalendarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+
+extension CalendarViewController: FSCalendarDataSource, FSCalendarDelegate {
+    
+    
+}
---
+++ Pods/Target Support Files/Pods-CalendarPlus-CalendarPlusUITests/Pods-CalendarPlus-CalendarPlusUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CalendarPlus_CalendarPlusUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CalendarPlus_CalendarPlusUITests
+@end
